9fa879441350f99bf1dc4448c9171bd9ac1e169f
laporka <125121347+laporka@users.noreply.github.com>
2023-06-25 10:46:14 +0300

    Add files via upload

---
+++ account/account.js
@@ -0,0 +1,71 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+
+
+function show() { 
+	document.getElementById("mainFrameOne").style.display="none"; 
+	document.getElementById("mainFrameTwo").style.display="block"; 
+}
+function showBack() { 
+	document.getElementById("mainFrameOne").style.display="block"; 
+	document.getElementById("mainFrameTwo").style.display="none"; 
+}
+
+function confirmm() {
+  document.getElementById("mainFrameOne").style.display="block"; 
+  document.getElementById("mainFrameTwo").style.display="none"; 
+
+  // SECONDNAME
+  var getSecond = document.getElementById('getSecondName').value;
+  document.getElementById('secondName').innerHTML = getSecond;
+
+  // MALEFEMALE
+  var getMalefemale = document.getElementById('getMaleFemale').value;
+  document.getElementById('MaleFemale').innerHTML = getMalefemale;
+
+  // NAME
+  var getIname = document.getElementById('getName').value;
+  document.getElementById('Name').innerHTML = getIname;
+
+  // FATHERNAME
+  var getFathername = document.getElementById('getFatherName').value;
+  document.getElementById('FatherName').innerHTML = getFathername;
+
+  // DATE
+  var getdate = document.getElementById('getBirthsday').value;
+  document.getElementById('Date').innerHTML = getdate;
+
+  // LANGUAGE
+  var getlanguage = document.getElementById('getLanguage').value;
+  document.getElementById('Language').innerHTML = getlanguage;
+}
