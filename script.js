ca68b543b238e2939a36602ef23aa33e06e6181b
laporka <125121347+laporka@users.noreply.github.com>
2023-07-20 00:45:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -78,8 +78,92 @@ function hidepopupCart() {
   document.getElementById('popup-cart').style.visibility="hidden";
 }
 
-AOS.init();
 
+// ДЛЯ СПЕНА
+// function totalClick(click){
+//   const totalClicks = document.getElementById('totalClicks');
+//   const sumvalue = parseInt(totalClicks.innerHTML) + click;
+//   totalClicks.innerHTML = sumvalue;
+// }
+
+// FIRST CARD
+function increment() {
+  document.getElementById('totalClicks').stepUp();
+  var getKilkist1 = document.getElementById('totalClicks').value;
+  document.getElementById('firstKilkist').innerHTML = getKilkist1;
+
+
+  // let test = document.getElementById('firstoutputSuma').innerHTML + document.getElementById('firstoutputSuma').innerHTML;
+  // document.getElementById('firstoutputSuma').innerHTML = test 
+  // console.log(test);
+
+}
+
+function decrement() {
+  document.getElementById('totalClicks').stepDown();
+  var getKilkist1 = document.getElementById('totalClicks').value;
+  document.getElementById('firstKilkist').innerHTML = getKilkist1;
+}
+
+
+// SECOND CARD
+function incrementSecond() {
+  document.getElementById('totalClicks2').stepUp();
+  var getKilkist1 = document.getElementById('totalClicks2').value;
+  document.getElementById('secondKilkist').innerHTML = getKilkist1;
+}
+
+function decrementSecond() {
+  document.getElementById('totalClicks2').stepDown();
+  var getKilkist1 = document.getElementById('totalClicks2').value;
+  document.getElementById('secondKilkist').innerHTML = getKilkist1;
+}
+
+
+// Third CARD
+function incrementThird() {
+  document.getElementById('totalClicks3').stepUp();
+  var getKilkist1 = document.getElementById('totalClicks3').value;
+  document.getElementById('thirdKilkist').innerHTML = getKilkist1;
+}
+
+function decrementThird() {
+  document.getElementById('totalClicks3').stepDown();
+  var getKilkist1 = document.getElementById('totalClicks3').value;
+  document.getElementById('thirdKilkist').innerHTML = getKilkist1;
+}
+
+
+// Fourth CARD
+function incrementFourth() {
+  document.getElementById('totalClicks4').stepUp();
+  var getKilkist1 = document.getElementById('totalClicks4').value;
+  document.getElementById('fourthKilkist').innerHTML = getKilkist1;
+}
+
+function decrementFourth() {
+  document.getElementById('totalClicks4').stepDown();
+  var getKilkist1 = document.getElementById('totalClicks4').value;
+  document.getElementById('fourthKilkist').innerHTML = getKilkist1;
+}
+
+
+// Fourth CARD
+function incrementFive() {
+  document.getElementById('totalClicks5').stepUp();
+  var getKilkist1 = document.getElementById('totalClicks5').value;
+  document.getElementById('fiveKilkist').innerHTML = getKilkist1;
+}
+
+function decrementFive() {
+  document.getElementById('totalClicks5').stepDown();
+  var getKilkist1 = document.getElementById('totalClicks5').value;
+  document.getElementById('fiveKilkist').innerHTML = getKilkist1;
+}
+
+
+
+AOS.init();
 
 
 
