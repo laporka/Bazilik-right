0b2a83e5013e4d9bac074fa61154c697967c7d94
laporka <125121347+laporka@users.noreply.github.com>
2023-07-30 13:34:40 +0300

    Add files via upload

--- all.js
+++ all.js
@@ -64,3 +64,88 @@ function decreaseQuantity(index) {
         updateQuantity(index); // Оновлюємо відображення кількості товару
     }
 }
+
+
+const radioInput = document.getElementById('zarazoplata');
+const divElement = document.querySelector('.oplata-zaraz');
+const contentElement = document.querySelector('.container-oplata');
+
+// Функція для встановлення стилів діва та контенту при зміні стану радіо-кнопки
+function setDivStyles() {
+  if (radioInput.checked) {
+    divElement.style.border = '1px solid #CBC9C4';
+    divElement.style.padding = '16px';
+    contentElement.style.display = 'block';
+  } else {
+    divElement.style.border = 'none';
+    divElement.style.padding = '0';
+    contentElement.style.display = 'none';
+  }
+}
+
+setDivStyles();
+
+// Додаємо обробник події change до радіо-кнопки
+radioInput.addEventListener('change', setDivStyles);
+
+
+
+
+const radioInputukr = document.getElementById('ukrpost');
+const divElementukr = document.querySelector('.container-Ukrpost');
+const contentElementukr = document.querySelector('.Ukrpost-content');
+
+// Функція для встановлення стилів діва та контенту при зміні стану радіо-кнопки
+function setDivStylesukr() {
+  if (radioInputukr.checked) {
+    divElementukr.style.border = '1px solid #CBC9C4';
+    divElementukr.style.padding = '16px';
+    contentElementukr.style.display = 'block';
+  } else {
+    divElementukr.style.border = 'none';
+    divElementukr.style.padding = '0';
+    contentElementukr.style.display = 'none';
+  }
+}
+
+setDivStylesukr();
+
+// Додаємо обробник події change до радіо-кнопки
+radioInputukr.addEventListener('change', setDivStylesukr);
+
+
+// NOVAPOST
+const radioInputnova = document.getElementById('newpost');
+const divElementnova = document.querySelector('.container-Newpost');
+const contentElementnova = document.querySelector('.Newpost-content');
+
+// Функція для встановлення стилів діва та контенту при зміні стану радіо-кнопки
+function setDivStylesnova() {
+  if (radioInputnova.checked) {
+    divElementnova.style.border = '1px solid #CBC9C4';
+    divElementnova.style.padding = '16px';
+    contentElementnova.style.display = 'block';
+  } else {
+    divElementnova.style.border = 'none';
+    divElementnova.style.padding = '0';
+    contentElementnova.style.display = 'none';
+  }
+}
+
+setDivStylesnova();
+
+// Додаємо обробник події change до радіо-кнопки
+radioInputnova.addEventListener('change', setDivStylesnova);
+
+
+
+
+function moveToNextInput(event, nextInputId) {
+  const currentInput = event.target;
+  const nextInput = document.getElementById(nextInputId);
+  if (nextInput && nextInput.tagName === 'INPUT') {
+      if (currentInput.value.length >= currentInput.maxLength) {
+          nextInput.focus();
+      }
+  }
+}
---
+++ nav.js
@@ -0,0 +1,118 @@
+const items = document.querySelectorAll(".nav-bar li");
+    items.forEach((item) => {
+      item.addEventListener("click", () => {
+        document.querySelector("li.active").classList.remove("active");
+        item.classList.add("active");
+      });
+    });
+
+
+// COLORS
+const colors = document.querySelectorAll(".choose-color li");
+colors.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector("li.active-color").classList.remove("active-color");
+    item.classList.add("active-color");
+  });
+});
+
+
+let quantity = 1;   // Змінна для збереження кількості товару (початкове значення 1)
+
+function increaseQuantity(index) {
+    const quantityInput = document.getElementById(`quantity${index}`);
+    quantity = parseInt(quantityInput.value);
+    quantity++;
+    quantityInput.value = quantity;
+    updateTotalPrice(index);
+}
+
+function decreaseQuantity(index) {
+    const quantityInput = document.getElementById(`quantity${index}`);
+    quantity = parseInt(quantityInput.value);
+    if (quantity > 1) {
+        quantity--;
+        quantityInput.value = quantity;
+        updateTotalPrice(index);
+    }
+}
+
+
+
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("slide-number");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("inside-radio", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " inside-radio";
+}
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("slide-number");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("inside-radio", "");
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " inside-radio";
+  setTimeout(carousel, 6000); // Change image every 2 seconds
+}
+
+
+
+
+function goBack() {
+    sessionStorage.setItem('previousPage', window.location.href);
+    history.back();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const previousPage = sessionStorage.getItem('previousPage');
+
+    if (previousPage && previousPage !== window.location.href) {
+        sessionStorage.removeItem('previousPage');
+
+        window.location.href = previousPage;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
